b65bbceecb2d79d6c69733bf920e1cee9287e718
stockrenz55-cyber <stockrenz55@gmail.com>
2026-02-03 06:27:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 module.exports = {
   PORT: 3000,
 
-  // GANTI & JANGAN SEBAR
-  JWT_SECRET: "GANTI_DENGAN_SECRET_PANJANG_RANDOM",
+  JWT_SECRET: "GANTI_DENGAN_RANDOM_PANJANG_&_AMAN",
 
-  OWNER: {
-    username: "owner",
-    password: "owner123"
-  },
+  OWNER_USER: "owner",
+  OWNER_PASS: "owner123",
 
-  PTERO: {
-    DOMAIN: "https://nyxostudio.web.id",
-    API_KEY: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
-    CLIENT_KEY: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
-    EGG: 15,
-    NEST: 5,
-    LOC: 1
-  }
+  PTERO_DOMAIN: "https://nyxostudio.web.id",
+  PTERO_APIKEY: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
+  PTERO_CLIENT_KEY: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
+
+  EGG: 15,
+  NEST: 5,
+  LOC: 1
 };
