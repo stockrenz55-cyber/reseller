7de9c9f9b210f447e0f56b352b7ebbe86a2baa2a
stockrenz55-cyber <stockrenz55@gmail.com>
2026-02-03 06:13:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,12 @@
 module.exports = {
-  app: {
-    port: 3000,
-    sessionSecret: "super-secret-neon-session",
-  },
+  PORT: 3000,
+  JWT_SECRET: "GANTI_SECRET_INI",
 
-  pterodactyl: {
-    domain: "https://nyxostudio.web.id",
-    apikey: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
-    capikey: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
-    egg: 15,
-    nestid: 5,
-    loc: 1,
-  },
+  domain: "https://nyxostudio.web.id",
+  apikey: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
+  capikey: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
 
-  roles: {
-    OWNER: "owner",
-    ADMIN: "admin",
-    RESELLER: "reseller",
-  },
-
-  permissions: {
-    owner: ["create_user", "delete_user", "create_panel", "delete_panel", "list_panel"],
-    admin: ["create_user", "create_panel", "delete_panel", "list_panel"],
-    reseller: ["create_panel", "list_panel"],
-  }
+  egg: 15,
+  nestid: 5,
+  loc: 1
 };
