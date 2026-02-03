3f73332505195c19172609f0f4d5f0c5d81be788
stockrenz55-cyber <stockrenz55@gmail.com>
2026-02-03 08:22:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
-module.exports = {
-  PORT: 3000,
+// ########## PANEL CONFIG ##########
+const CONFIG = {
+  domain: "https://nyxostudio.web.id",
+  apikey: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
+  capikey: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
 
-  JWT_SECRET: "GANTI_DENGAN_RANDOM_PANJANG_&_AMAN",
-
-  OWNER_USER: "owner",
-  OWNER_PASS: "owner123",
-
-  PTERO_DOMAIN: "https://nyxostudio.web.id",
-  PTERO_APIKEY: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
-  PTERO_CLIENT_KEY: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
-
-  EGG: 15,
-  NEST: 5,
-  LOC: 1
+  egg: 15,
+  nestid: 5,
+  loc: 1
 };
