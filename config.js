ec677ef4c275047e83d9f1154754101a4c2666a8
stockrenz55-cyber <stockrenz55@gmail.com>
2026-02-03 08:35:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-// ########## PANEL CONFIG ##########
-const CONFIG = {
+module.exports = {
   domain: "https://nyxostudio.web.id",
   apikey: "ptla_mIoA2uJSLrSVm0r3l21x2wTjQee9ZpCR5YjifRufQue",
   capikey: "ptlc_0nPji2uEXI9WuSU6mRmBwwyG8C1TV2yen0eTGkO52Vc",
-
   egg: 15,
   nestid: 5,
   loc: 1
